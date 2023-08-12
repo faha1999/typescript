588f9468bb7d0e3e2a5ea8bd93fce7ffa70472ae
Kawsar Ahmed Fahad <54038576+faha1999@users.noreply.github.com>
2023-08-12 23:46:29 +0600

    complete explicit & Union

--- output/script.js
+++ output/script.js
@@ -37,3 +37,27 @@ let person = {
 };
 person.name = 'Sakib';
 console.log(person);
+// explicit & Union //
+let a;
+let b;
+a = 'faha1999';
+b = 3;
+//
+let c = [];
+c.push('faha1999');
+//
+let d = [];
+d.push('Faha1999', 4);
+// normal variable
+let e;
+e = 'faha';
+e = 4;
+// object
+let f;
+f = { name: 'faha1999' };
+//
+let g;
+g = { name: 'faha1999', age: 24, adult: false };
+//
+let h;
+h: [1, 2, 3];
--- src/script.ts
+++ src/script.ts
@@ -58,3 +58,48 @@ let person = {
 person.name = 'Sakib';
 
 console.log(person);
+
+// explicit & Union //
+
+let a: string;
+let b: number;
+
+a = 'faha1999';
+
+b = 3;
+
+//
+
+let c: string[] = [];
+c.push('faha1999');
+
+//
+
+let d: (string | number)[] = [];
+d.push('Faha1999', 4);
+
+// normal variable
+let e: string | number;
+
+e = 'faha';
+e = 4;
+
+// object
+let f: object;
+
+f = { name: 'faha1999' };
+
+//
+
+let g: {
+  name: string;
+  age: number;
+  adult: boolean;
+};
+
+g = { name: 'faha1999', age: 24, adult: false };
+
+//
+let h: object;
+
+h: [1, 2, 3];
