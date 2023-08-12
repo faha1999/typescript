429d67d656a09b7f1bf7dc215d24dd946ef0a838
Kawsar Ahmed Fahad <54038576+faha1999@users.noreply.github.com>
2023-08-12 22:31:28 +0600

    complete type basic

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,60 @@
 // console.log('hello');
 
-const country = 'I love Bangladesh';
+// const country = 'I love Bangladesh';
 
-console.log(country); 
+// console.log(country);
+
+// let playerName= 'Mashrafi';
+// let age= 34;
+
+// console.log(playerName);
+
+// age =34;
+
+// console.log(playerName);
+
+// let playerName;
+
+// playerName ='Mashrafi';
+
+// playerName=34;
+
+// console.log(playerName);
+
+// function //
+
+// function multiply(a: number, b: number) {
+//   return a * b;
+// }
+
+// console.log(multiply(3, 4));
+
+// array //
+let fruits = ['apple', 'banana', 'orange'];
+
+fruits.push('graphs');
+
+console.log(fruits);
+
+// mixed array //
+let mixed = ['apple', 3, true, {}];
+
+mixed.push('shkfs');
+mixed.push(false);
+mixed.push(5);
+mixed.push({
+  name: 'Banana',
+});
+
+console.log(mixed);
+
+// Object //
+let person = {
+  name: 'Mashrafi',
+  age: 35,
+  isCaptain: true,
+};
+
+person.name = 'Sakib';
+
+console.log(person);
