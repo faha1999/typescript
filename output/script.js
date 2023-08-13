4b72a969fdb9fd6bd6f07fc0a915bc0a41e767ad
Kawsar Ahmed Fahad <54038576+faha1999@users.noreply.github.com>
2023-08-13 22:06:15 +0600

    Type Aliases done

--- output/script.js
+++ output/script.js
@@ -91,3 +91,9 @@ const myFunctionReturn = (a, b) => {
     return a + b;
 };
 myFunctionReturn('wor', 'ld');
+const userDetails = (id, user) => {
+    console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`);
+};
+const sayHello = (user) => {
+    console.log(`Hello ${user.age > 50 ? 'Sir' : 'Mr'} ${user.name}`);
+};
--- src/script.ts
+++ src/script.ts
@@ -151,3 +151,15 @@ const myFunctionReturn = (a: string, b: string) => {
 };
 
 myFunctionReturn('wor', 'ld');
+
+// Type Aliases
+type stringOrNum = string | number;
+type userType = { name: string; age: number };
+
+const userDetails = (id: stringOrNum, user: userType) => {
+  console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`);
+};
+
+const sayHello = (user: userType) => {
+  console.log(`Hello ${user.age > 50 ? 'Sir' : 'Mr'} ${user.name}`);
+};
